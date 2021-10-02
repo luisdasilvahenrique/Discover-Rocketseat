7fd55e5bd546a1c077de983e7d2e7895c1be248b
Luís Henrique <luisenriklh1177027@gmail.com>
2021-10-01 22:57:35 -0300

    formating of currency and addition in sum inside incomes and expenses

--- scripts.js
+++ scripts.js
@@ -1,84 +1,99 @@
-    const Modal = {
-      open(){
-        //abrir modal
-        //ativar a classe active do modal
-        document
-        .querySelector('.modal-overlay')
-        .classList.add('active')
-        },
-      close(){
-          //fechar modal
-          //remover a classe active do modal
-          document
-          .querySelector('.modal-overlay')
-          classList.remove('active')
-        }
-        //teste
-    }
-    const transaction = [{
-      id: 1,
-      description: 'Luz',
-      amount: -50012,
-      date: "23/01/2021",
-    },
-    {
-      id: 2,
-      description: 'Website',
-      amount: -50003,
-      date: "23/01/2021",
-    },
-    {
-      id: 3,
-      description: 'Internet',
-      amount: -20000,
-      date: "23/01/2021",
-    },
-    {
-      id: 4,
-      description: 'Botijão',
-      amount: 10000,
-      date: "23/01/2021",
-    },
-    {
-      id: 5,
-      description: 'Botijão',
-      amount: 20000,
-      date: "23/01/2021",
-    }
-  ]
-
-    const Transaction = {
-      incomes(){
-        //pegar todas as transações
-        //verificar se a transação é maior que zero
-        //somar a uma váriavel e retornar a variável
-        // 1:23:00
-        return 'Cheguei'
-      },
-      expenses(){
-        return 'Aqui'
-      },
-      total(){
-        return 'Discover'
+const Modal = {
+  open() {
+    //abrir modal
+    //ativar a classe active do modal
+    document
+      .querySelector('.modal-overlay')
+      .classList.add('active')
+  },
+  close() {
+    //fechar modal
+    //remover a classe active do modal
+    document
+      .querySelector('.modal-overlay')
+    classList.remove('active')
+  }
+  //teste
+}
+const transaction = [
+  {
+  id: 1,
+  description: 'Luz',
+  amount: -30012,
+  date: "23/01/2021",
+},
+{
+  id: 2,
+  description: 'Website',
+  amount: -30003,
+  date: "24/11/2021",
+},
+{
+  id: 3,
+  description: 'Internet',
+  amount: -20000,
+  date: "05/03/2021",
+},
+{
+  id: 4,
+  description: 'Botijão',
+  amount: 10000,
+  date: "11/11/2021",
+},
+{
+  id: 5,
+  description: 'Botijão',
+  amount: 20000,
+  date: "114/06/2021",
+}
+]
+// 1:30:45 Pode haver erro no "expenses" na saída e soma dos valores
+const Transaction = {
+  incomes() {
+    let income = 0;
+    //pegar todas as transações
+    //para cada transação
+    transaction.forEach(transaction => {
+      //somar a uma váriavel e retornar a variável
+      if (transaction.amount > 0) {
+        income = income + transaction.amount;
       }
-    }
+    })
+    //para cada transação, se for maior que zero
+    return income;
+  },
 
-    const DOM = {
-      transactionContainer: document.querySelector('#data-table tbody'),
+  expenses() {
+    let expense = 0;
+    transaction.forEach(transaction => {
+      if (transaction < 0) {
+        expense += transaction.amount;
+      }
+    })
+    return expense;
+  },
 
-      addTransaction(transaction, index){
-        const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+  total() {
+    return Transaction.incomes() + Transaction.expenses();
+  }
+}
 
-        DOM.transactionContainer.appendChild(tr)
+const DOM = {
+  transactionContainer: document.querySelector('#data-table tbody'),
 
-      },
-        innerHTMLTransaction(transaction) {
-          const CssClass = transaction.amount > 0 ? "income" : "expense"
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
 
-          const amount = Utils.formatCurrency(transaction.amount)
+    DOM.transactionContainer.appendChild(tr)
 
-          const html = `
+  },
+  innerHTMLTransaction(transaction) {
+    const CssClass = transaction.amount > 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
           <td class=description>${transaction.description}</td>
           <td class="${CssClass}">${amount}</td>
           <td class="date">${transaction.date}</td>
@@ -86,46 +101,46 @@
             <img src="./assets/minus.svg" alt="Remover transação">
           </td>
           `
-          return html
-      },
+    return html
+  },
 
-      updateBalance() {
-          document
-          .getElementById('incomeDisplay')
-          .innerHTML = Transaction.incomes()
-              
-          document
-          .getElementById('expenseDisplay')
-          .innerHTML = Transaction.expenses()
-          document
-          .getElementById('totalDisplay')
-          .innerHTML = Transaction.total()
+  updateBalance() {
+    document
+      .getElementById('incomeDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.incomes())
 
-      },
+    document
+      .getElementById('expenseDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.expenses())
+    document
+      .getElementById('totalDisplay')
+      .innerHTML = Utils.formatCurrency(Transaction.total())
 
-    }
+  },
+
+}
 
 // formantando moeda
-    const Utils = {
-      formatCurrency(value){
-        const signal  = Number(value) < 0 ? "-" : ""
-      
-        value = String(value).replace(/\D/g, "")
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
 
-        value = Number(value) / 100
+    value = String(value).replace(/\D/g, "")
 
-        value = value.toLocaleString("pt-BR", {
-          style: "currency",
-          currency: "BRL"
-        })
+    value = Number(value) / 100
 
-        return signal + value
-      }
-    }
-
-    transaction.forEach(function(transation){
-      DOM.addTransaction(transation)
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
     })
 
-    DOM.updateBalance()
-    
+    return signal + value
+  }
+}
+
+transaction.forEach(function (transation) {
+  DOM.addTransaction(transation);
+})
+
+DOM.updateBalance()
+
